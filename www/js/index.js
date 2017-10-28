9716165d8613e0c0dab25d05bea4f0bcd50087d8
Aleksandar Ivanov <smichess@gmail.com>
2017-10-28 16:21:21 +0300

    hooking leaflet to s-57 map

--- www/js/index.js
+++ www/js/index.js
@@ -1,7 +1,9 @@
 var app = {
     // Application Constructor
     initialize: function() {
+        this.receivedEvent.bind(this);
         document.addEventListener('deviceready', this.onDeviceReady.bind(this), false);
+        document.addEventListener('DOMContentLoaded', this.onDOMContentLoaded.bind(this), false);
     },
 
     // Bind any cordova events here. Common events are:
@@ -10,9 +12,21 @@ var app = {
         this.receivedEvent('deviceready');
     },
 
+    onDOMContentLoaded: function() {
+        this.receivedEvent('DOMContentLoaded');
+    },
     // Update DOM on a Received Event
     receivedEvent: function(id) {
-        
+        if(id === "deviceready" || id === "DOMContentLoaded"){
+            this.initLeaflet();
+        }
+    },
+
+    initLeaflet: function(){
+        var map = L.map('map').setView([61.6894343, 27.2789637], 15);
+        var wmsLayer = L.tileLayer.wms('http://kartta.liikennevirasto.fi/meriliikenne/dgds/wms_ip/merikartta?', {
+        layers: 'background,cities,cells'
+            }).addTo(map);
     }
 };
 
